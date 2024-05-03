33e1d344165325a2d316602b5ebad3435e989c29
NECIB AMARA <amaracoder39@gmail.com>
2024-05-03 20:15:18 +0100

    Refactor DELETE route in airport route to handle missing fields and add response status code

--- src/app/api/airport/route.ts
+++ src/app/api/airport/route.ts
@@ -24,3 +24,18 @@ export const POST = async (req: NextRequest) => {
         return NextResponse.json({error: "Error here"},{status: 500});
     }
 }
+
+export const DELETE = async (req: NextRequest) => {
+    const id = req.nextUrl.searchParams.get("id");
+    if(!id) return NextResponse.json({success: false,error: "Missing fields"}, {status: 400});
+    try{
+        await prisma.airport.delete({
+            where: {
+                id: parseInt(id as string)
+            }
+        });
+        return NextResponse.json({success: true}, {status: 200});
+    }catch(error){
+        return NextResponse.json({success: false,error: "Error here"},{status: 500});
+    }
+}
