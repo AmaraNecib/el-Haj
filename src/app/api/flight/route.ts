774050fd4dbc2448abda9f00a651785c71bd7323
NECIB AMARA <amaracoder39@gmail.com>
2024-05-03 21:16:21 +0100

    Refactor flight POST route to handle errors and update variable names in route.ts

--- src/app/api/flight/route.ts
+++ src/app/api/flight/route.ts
@@ -15,29 +15,31 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
     // @ts-ignore
     try{ 
     let { to,price,timeOfFlight,airportId,startTime,endTime } = await req.json();
-    let airport = await prisma.airport.findUnique({where: {id: airportId}});
+    const airport = await prisma.airport.findUnique({where: {id: airportId}});
     if(!airport) return NextResponse.json({message: "Airport not found"}, {status: 404});
     timeOfFlight = parseInt(timeOfFlight);
     price = parseInt(price);
     startTime = new Date(startTime);
     endTime = new Date(endTime);
-    const flight = await prisma.flight.create({
-        // @ts-ignore
-        data: {
-            from: airport.city,
-            to,
-            price,
-            timeOfFlight,
-            endTime,
-            startTime,
-            airport: {
-                connect: {
-                    id: parseInt(airportId)
-                }
-            }
-        }
-    });
-    return NextResponse.json({flight}, {status: 201});}
+    return NextResponse.json({to,price,timeOfFlight,airportId,startTime,endTime},{status: 500});
+    // const flight = await prisma.flight.create({
+    //     // @ts-ignore
+    //     data: {
+    //         from: airport.city,
+    //         to,
+    //         price,
+    //         timeOfFlight,
+    //         endTime,
+    //         startTime,
+    //         airport: {
+    //             connect: {
+    //                 id: parseInt(airportId)
+    //             }
+    //         }
+    //     }
+    // });
+    // return NextResponse.json({flight}, {status: 201});
+}
     catch(error){
         return NextResponse.json({message: "Error"},{status: 500});
     }
