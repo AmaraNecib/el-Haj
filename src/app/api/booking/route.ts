faa30ddfafb6e293c8679760c278e1f7d7e12145
NECIB AMARA <amaracoder39@gmail.com>
2024-05-03 20:56:51 +0100

    Refactor booking route to remove unnecessary code and update POST route

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -3,22 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest, res: NextResponse) => {
     // get id from url query
-    const id = parseInt(await req.url.split("/")[5]);
   const bookings = await prisma.booking.findMany({
-    where: {
-      id,
-    },
   });
   return NextResponse.json(bookings);
 };
 export const POST = async (req: NextRequest, res: NextResponse) => {
   // @ts-ignore
-  const { startTime, endTime, statusId, flightId, userId } = req.json();
+  const { statusId, flightId, userId } = req.json();
   const booking = await prisma.booking.create({
     // @ts-ignore
     data: {
-      startTime,
-      endTime,
       statusId,
       flightId,
       userId,
