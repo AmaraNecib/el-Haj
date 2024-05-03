f2bbf7cd23cbb39a7a60f8a39c20f65209fe24d5
NECIB AMARA <amaracoder39@gmail.com>
2024-05-03 18:20:45 +0100

    Refactor login route to handle password validation using bcrypt

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,17 +1,20 @@
 import { prisma } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { login } from "@/lib/auth";
+import { compare } from "bcrypt";
 export const POST = async (req: NextRequest, res: NextResponse) => {
     const { email, password } = await req.json();
-    const user = await prisma.user.findFirst({
+    const user = await prisma.user.findUnique({
         where: {
             email,
-            password
         }
     });
+
     if (!user) {
         return NextResponse.json({ success: false, message: "Invalid credentials" }, { status: 401 });
     }
+    const valid = await compare(password, user.password);
+    if(!valid) return NextResponse.json({success: false, message: "Invalid credentials"}, {status: 401});
     const token = await login(user.id)
     return NextResponse.json({success: true, token}, {status: 200})
 }
